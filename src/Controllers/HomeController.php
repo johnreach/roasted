c1c462de43559453e4fde2ab93e7d46ab0170d11
jacob.goodwin <jacob.goodwin@smail.astate.edu>
2016-04-26 02:07:20 -0400

    Testing

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -9,11 +9,13 @@
 namespace Roasted\Controllers;
 
 use \Roasted\Controllers\Controller;
+use \Roasted\Models\User;
 
 class HomeController extends Controller {
     
     public function index($request, $response) {
         
+        $user = User::find_one(1);
         return $this->view->render($response, 'index.html');
     }
   
